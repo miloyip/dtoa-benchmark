58089b985c1dee7cd1b40cd6436f42798ff6bd0c
Leo Yuriev <leo@yuriev.ru>
2019-04-12 16:53:34 +0300

    add erthink_test.

---
+++ src/erthink_test.cpp
@@ -0,0 +1,13 @@
+﻿#if __cplusplus >= 201103L
+
+#include "test.h"
+#include "erthink/erthink_d2a.h"
+
+void dtoa_erthink(double v, char* buffer)
+{
+	*erthink::d2a(v, buffer) = '\0';
+}
+
+REGISTER_TEST(erthink);
+
+#endif
