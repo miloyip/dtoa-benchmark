5bc65c29b88a14bf8672b588be8dd9cf2090f721
Victor Zverovich <victor.zverovich@gmail.com>
2019-04-20 08:20:06 -0700

    Add terminating nul in double-conversion benchmark

--- src/doubleconvtest.cpp
+++ src/doubleconvtest.cpp
@@ -6,6 +6,7 @@ using namespace double_conversion;
 void dtoa_doubleconv(double value, char* buffer) {
 	StringBuilder sb(buffer, 26);
 	DoubleToStringConverter::EcmaScriptConverter().ToShortest(value, &sb);
+	sb.Finalize();
 }
 
 REGISTER_TEST(doubleconv);
