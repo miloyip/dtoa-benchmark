6189a90bd15a1ac0b3082f6f4112695a4efaaf0c
Leonid Yuriev <leo@yuriev.ru>
2019-05-31 13:31:19 +0300

    reformat & fix includes

--- src/milo/dtoa_milo.h
+++ src/milo/dtoa_milo.h
@@ -1,6 +1,7 @@
-#pragma once
+﻿#pragma once
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 
 #if defined(_MSC_VER)
 #include "msinttypes/stdint.h"
--- stb_sprintf.cpp
+++ stb_sprintf.cpp
@@ -2,8 +2,8 @@
 #include "stb_sprintf/stb_sprintf.h"
 #include "test.h"
 
-void dtoa_stb_sprintf(double value, char* buffer) {
-	stbsp_sprintf(buffer, "%.17g", value);
+void dtoa_stb_sprintf(double value, char *buffer) {
+  stbsp_sprintf(buffer, "%.17g", value);
 }
 
 REGISTER_TEST(stb_sprintf);
