b4bd13ed479957db52b708589ea6557b3bb3bb6f
Leonid Yuriev <leo@yuriev.ru>
2020-06-21 21:07:44 +0300

    add erthink_shodan.

--- src/erthink_test.cpp
+++ src/erthink_test.cpp
@@ -7,3 +7,14 @@ char *dtoa_erthink(double v, char *const buffer) {
 }
 
 REGISTER_TEST(erthink);
+
+char *dtoa_erthink_shodan(double v, char *const buffer) {
+  erthink::grisu::shodan_printer<true> printer(
+      buffer, buffer + erthink::grisu::shodan_printer<>::buffer_size);
+  erthink::grisu::convert(printer, v);
+  const auto begin_end = printer.finalize_and_get();
+  *begin_end.second = '\0';
+  return begin_end.first;
+}
+
+REGISTER_TEST(erthink_shodan);
